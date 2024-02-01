4f6b11ac59c3ad7093f28d680eb20868ba0780ef
Simon Emanuel Schmid <simon@schmid.io>
2024-02-01 14:04:01 +0000

    Trying to bust cache but failing

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,21 @@ export const metadata: Metadata = {
   title: "Farcaster CryptoPunks The Graph Frame",
   description: "Powered by The Graph",
   openGraph: {
-    images: [`https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/og`],
+    images: [
+      `https://${
+        process.env.NEXT_PUBLIC_VERCEL_URL
+      }/api/og?timestamp=${new Date().getTime()}`,
+    ],
   },
   other: {
     "fc:frame": "vNext",
-    "fc:frame:image": `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/og`,
-    "fc:frame:button:1": "Green",
-    "fc:frame:button:2": "Purple",
-    "fc:frame:button:3": "Red",
-    "fc:frame:button:4": "Blue",
+    "fc:frame:image": `https://${
+      process.env.NEXT_PUBLIC_VERCEL_URL
+    }/api/og?timestamp=${new Date().getTime()}`,
+    "fc:frame:button:1": "Refresh",
+    "fc:frame:post_url": `https://${
+      process.env.NEXT_PUBLIC_VERCEL_URL
+    }?timestamp=${new Date().getTime()}`,
   },
 };
 
@@ -21,7 +27,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        Hello world asd
+        <img src="/api/og" />
       </div>
     </main>
   );
