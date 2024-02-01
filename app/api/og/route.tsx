0c4b1d78cdd7b2032ee330fe73242fad6b719eb6
Simon Emanuel Schmid <simon@schmid.io>
2024-02-01 13:10:11 +0000

    Use graphql-requests to send the request

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -1,10 +1,45 @@
+import { request, GraphQLClient, gql } from "graphql-request";
 import { ImageResponse } from "next/og";
-// App router includes @vercel/og.
-// No need to install it.
 
 export const runtime = "edge";
 
+const GRAPHQL_ENDPOINT = `https://gateway-arbitrum.network.thegraph.com/api/${process.env.THE_GRAPH_API_KEY}/subgraphs/id/2hTKKMwLsdfJm9N7gUeajkgg8sdJwky56Zpkvg8ZcyP8`;
+
 export async function GET() {
+  const document = gql`
+    {
+      sales(orderBy: timestamp, orderDirection: desc, first: 1) {
+        amount
+        nft {
+          tokenId
+          ... on Punk {
+            id
+            metadata {
+              contractURI
+              id
+              tokenId
+              tokenURI
+              svg
+              traits {
+                id
+                type
+              }
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  const graphQLClient = new GraphQLClient(GRAPHQL_ENDPOINT, {
+    fetch,
+  });
+  // const response = await request(``, document);
+
+  const response = await graphQLClient.request(document);
+
+  console.log(response);
+
   return new ImageResponse(
     (
       <div
